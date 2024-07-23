1c7edcfc6f96180cdecda8a174b3a4d2fe4998f7
Kelsey Wimberley <klwimberley@gmail.com>
2024-07-22 23:36:03 -0700

    Button prefab updated for poke

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadEnding()
+    {
+        SceneManager.LoadScene("Ending");
+    }
+}
