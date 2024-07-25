bcd353a17665289075fa8a5c4cc599af399b9494
Kelsey Wimberley <klwimberley@gmail.com>
2024-07-24 20:38:13 -0700

    Doors can rise

---
+++ Assets/Scripts/CellDoorRise.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellDoorRise : MonoBehaviour
+{
+    private PlayerControls sPlayerControls;
+    private bool inHershelRoom;
+    public float maxHeight;
+
+    private bool rise;
+    private float speed;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        sPlayerControls = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControls>();
+
+        inHershelRoom = false;
+        if (transform.position.z < 7.5f)
+        {
+            inHershelRoom = true;
+        }
+
+        rise = false;
+        speed = 5.0f;
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (rise)
+        {
+            if ((sPlayerControls.hershel && inHershelRoom) ||
+                (!sPlayerControls.hershel && !inHershelRoom))
+            {
+                audioSource.volume = sPlayerControls.inRoomVolume;
+            }
+            else
+            {
+                audioSource.volume = sPlayerControls.outRoomVolume;
+            }
+
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+
+            if (transform.position.y > maxHeight)
+            {
+                rise = false;
+                audioSource.Stop();
+            }
+        }
+    }
+
+    public void BeginRising()
+    {
+        rise = true;
+        audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public bool hershel;
+
+    public GameObject hershelPos;
+    public GameObject gretaPos;
+
+    public float inRoomVolume = 0.35f;
+    public float outRoomVolume = 0.75f;
+
+    void Start()
+    {
+        hershel = true;
+    }
+
+    void Update()
+    {
+        
+    }
+}
